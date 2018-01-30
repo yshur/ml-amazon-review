851b6c67bfc770888b8ca3dc615947e5264585b4
root <yair.shur@gmail.com>
2018-01-30 12:20:26 +0200

    read review text

---
+++ review.php
@@ -0,0 +1,11 @@
+<?php
+	//$fileName = '/var/log/mss/mss.log';
+	$fileName = '/var/www/ml-amazon-review/Musical_Instruments_5.json';
+	$host= 'reviewerID';	//$argv[1];
+	$scriptName = 'reviewerName';	// $argv[2];
+	$cmd = "grep $host.*.$scriptName $fileName|tail -1";
+	exec($cmd,$outJson);
+	$outArray = json_decode($outJson[0],true);	
+	echo $outArray['reviewText']."\n";
+	echo $outArray['overall']."\n";
+?>
