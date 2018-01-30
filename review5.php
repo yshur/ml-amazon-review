bfef1023c291eeb5d1f1b65137bbc5cd46d9fe4e
root <yair.shur@gmail.com>
2018-01-30 14:52:03 +0200

    read rate x

---
+++ review5.php
@@ -0,0 +1,16 @@
+<?php
+	$fileName = '/var/www/ml-amazon-review/Musical_Instruments_5.json';
+	$data = file($fileName);
+	$good = "";
+	$i = 0;
+	foreach ($data as $line)
+	{
+		$review = json_decode($line, true);
+		if($review["reviewText"]==5)
+		{
+			$good .= "{$i}-".$review["reviewText"]."\n"; 
+			$i++;
+		}
+	}
+	echo $good;
+?>
